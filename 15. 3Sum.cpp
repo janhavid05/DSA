48c49a271e7a95872ad732e3dc58976bf860d13d
janhavid05 <janhavideshmukh58@gmail.com>
2026-08-19 10:01:11 +0530

    Add 15 no problem

---
+++ 15. 3Sum.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+// <--Leetcode 15 3Sum-->
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums)
+     {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        for(int i=0;i<nums.size();i++)
+        {   
+            if(i>0 && nums[i]==nums[i-1] )
+            {
+                continue;
+            }
+            int j=i+1;
+            int k=n-1;
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum>0)
+                {
+                    k--;
+                }
+                else if(sum<0)
+                {
+                    j++;
+                }
+                else
+                {
+                   ans.push_back({nums[i],nums[j],nums[k]});
+                   j++;
+                   k--;
+                   while(j<k && nums[j]==nums[j-1])
+                   {
+                    j++;
+                   }
+                }  
+            }
+        }
+        return ans;
+    }
+};
